30464301c86f180bd98f0c3b264dc492141518d2
kajstene <kaja.stene@inmeta.no>
2018-03-15 14:36:30 +0100

    angular-routes created  presentation and show component

--- ariot18-placeholder/src/app/app.module.ts
+++ ariot18-placeholder/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PresenterComponent } from './presenter/presenter.component';
+import {RouterModule, Routes} from '@angular/router';
+import { ShowComponent } from './show/show.component';
 
+const appRoutes: Routes = [
+  { path: 'presenter', component: PresenterComponent },
+  { path: 'show/:id',      component: ShowComponent },
+
+  { path: '',
+    redirectTo: '/presenter',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresenterComponent,
+    ShowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
