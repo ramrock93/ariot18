e7def850c0066a766dfe6fa8b74dd4eb78039f6a
kajstene <kaja.stene@inmeta.no>
2018-03-16 17:53:45 +0100

    show component

--- ariot18-placeholder/src/app/show/show.component.ts
+++ ariot18-placeholder/src/app/show/show.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {PresenterService} from '../presenter.service';
+import {SlideData} from '../SlideData';
+import {ActivatedRoute, Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-show',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowComponent implements OnInit {
 
-  constructor() { }
+  prototypeUrls = [
+    'https://cdn.pixabay.com/photo/2018/02/26/16/30/eggs-3183410_1280.jpg',
+    'https://cdn.pixabay.com/photo/2018/02/27/07/36/nature-3184889_1280.jpg',
+    'https://cdn.pixabay.com/photo/2018/03/06/21/09/easter-3204589_1280.jpg'
+  ]
+  imgSrcUrl: string;
+  cnt = 0;
+  secondsPerSlide = 5;
+  slideData: SlideData;
+  sessionId: number;
+  maxSlides: number;
+  sub: any;
+  private interval: any;
 
+  constructor(private presenterService: PresenterService,
+              private route: ActivatedRoute,
+              private router: Router) {
+    this.imgSrcUrl = this.prototypeUrls[0];
+  }
   ngOnInit() {
+    this.maxSlides = 5;
+    this.interval = setInterval(() => {
+      this.getSlideData();
+    }, this.secondsPerSlide * 1000);
+
+    this.sub = this.route.params.subscribe(params => {
+      console.log(params)
+      this.sessionId = params['id'] * 1;
+    });
+  }
+  getSlideData(): void {
+    if ( this.sessionId ){
+      this.presenterService.getSlideData(this.sessionId)
+        .then((slidedata) => {
+          this.slideData = slidedata;
+          if (this.slideData.images) {
+            this.setUrl(this.slideData.images[0].url);
+          } else {
+            this.switchImage();
+          }
+        })
+        .catch(() => this.switchImage());
+    } else {
+      this.switchImage();
+    }
+  }
+  moveOn(): void {
+    if (this.cnt > this.maxSlides) {
+      console.log('navigate!');
+      clearInterval(this.interval);
+      this.router.navigate(['thankyou']);
+    }
   }
 
+  switchImage(): void {
+    this.setUrl(this.prototypeUrls[++this.cnt % 3]);
+  }
+  setUrl(url: string): void {
+    this.moveOn();
+    this.imgSrcUrl = url;
+  }
 }
