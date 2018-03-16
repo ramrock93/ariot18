99dab65580307a62180ba900a50e4086792b1e23
ramesfan <ramin.esfandiari@inmeta.com>
2018-03-16 14:58:39 +0100

    adds play button, posting event.

--- ariot18-placeholder/src/app/app.module.ts
+++ ariot18-placeholder/src/app/app.module.ts
@@ -1,36 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
 
-import { AppComponent } from './app.component';
-import { PresenterComponent } from './presenter/presenter.component';
-import {RouterModule, Routes} from '@angular/router';
-import { ShowComponent } from './show/show.component';
+import {AppComponent} from "./app.component";
+import {RouterModule, Routes} from "@angular/router";
+import {ShowComponent} from "./show/show.component";
+import {PresenterComponent} from "./presenter/presenter.component";
+
+import {AngularFontAwesomeModule} from "angular-font-awesome";
 
 const appRoutes: Routes = [
-  { path: 'presenter', component: PresenterComponent },
-  { path: 'show/:id',      component: ShowComponent },
+  {path: "presenter", component: PresenterComponent},
+  {path: "show/:id", component: ShowComponent},
 
-  { path: '',
-    redirectTo: '/presenter',
-    pathMatch: 'full'
+  {
+    path: "",
+    redirectTo: "/presenter",
+    pathMatch: "full"
   }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PresenterComponent,
-    ShowComponent
+    ShowComponent,
+    PresenterComponent
   ],
   imports: [
     BrowserModule,
+    AngularFontAwesomeModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true }
-      )
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
