dceec2472f45ad94cc2d46763b1ad5ba9e395f66
ramesfan <ramin.esfandiari@inmeta.com>
2018-03-17 09:38:19 +0100

    removes .valueOf() from backend response.

--- ariot18-placeholder/src/app/recorder.service.ts
+++ ariot18-placeholder/src/app/recorder.service.ts
@@ -10,12 +10,8 @@ export class RecorderService {
   }
 
   sendRecordData(sessionId: number, recordData: string) {
-    const headerOptions = new HttpHeaders();
-    headerOptions.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
-    headerOptions.append('Access-Control-Allow-Origin', 'http://localhost:4503');
-    headerOptions.append('Access-Control-Allow-Headers', "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
-    headerOptions.append("Content-Type", "application/json");
-    this.http.post(this.uploadTextUrl, {id: sessionId, text: recordData}, {headers: headerOptions})
-      .subscribe(res => console.log("Response from interaktivdemo", res.valueOf()));
+
+    this.http.post(this.uploadTextUrl, {id: sessionId, text: recordData})
+      .subscribe(res => console.log("Response from interaktivdemo", res));
   }
 }
